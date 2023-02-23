70c57b2c1325c56a06acb3afa5a612ddaae47cfb
JuanAriza17 <60227446+JuanAriza17@users.noreply.github.com>
2023-02-23 06:20:08 -0500

    Modificaciones App y Docs

--- App/controller.py
+++ App/controller.py
@@ -22,17 +22,150 @@
 
 import config as cf
 import model
+import time
 import csv
-
+import tracemalloc
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
 """
 
-# Inicialización del Catálogo de libros
+
+def new_controller():
+    """
+    Crea una instancia del modelo
+    """
+    #TODO: Llamar la función del modelo que crea las estructuras de datos
+    pass
+
 
 # Funciones para la carga de datos
 
+def load_data(control, filename):
+    """
+    Carga los datos del reto
+    """
+    # TODO: Realizar la carga de datos
+    pass
+
+
 # Funciones de ordenamiento
 
+def sort(control):
+    """
+    Ordena los datos del modelo
+    """
+    #TODO: Llamar la función del modelo para ordenar los datos
+    pass
+
+
 # Funciones de consulta sobre el catálogo
+
+def get_data(control, id):
+    """
+    Retorna un dato por su ID.
+    """
+    #TODO: Llamar la función del modelo para obtener un dato
+    pass
+
+
+def req_1(control):
+    """
+    Retorna el resultado del requerimiento 1
+    """
+    # TODO: Modificar el requerimiento 1
+    pass
+
+
+def req_2(control):
+    """
+    Retorna el resultado del requerimiento 2
+    """
+    # TODO: Modificar el requerimiento 2
+    pass
+
+
+def req_3(control):
+    """
+    Retorna el resultado del requerimiento 3
+    """
+    # TODO: Modificar el requerimiento 3
+    pass
+
+
+def req_4(control):
+    """
+    Retorna el resultado del requerimiento 4
+    """
+    # TODO: Modificar el requerimiento 4
+    pass
+
+
+def req_5(control):
+    """
+    Retorna el resultado del requerimiento 5
+    """
+    # TODO: Modificar el requerimiento 5
+    pass
+
+def req_6(control):
+    """
+    Retorna el resultado del requerimiento 6
+    """
+    # TODO: Modificar el requerimiento 6
+    pass
+
+
+def req_7(control):
+    """
+    Retorna el resultado del requerimiento 7
+    """
+    # TODO: Modificar el requerimiento 7
+    pass
+
+
+def req_8(control):
+    """
+    Retorna el resultado del requerimiento 8
+    """
+    # TODO: Modificar el requerimiento 8
+    pass
+
+
+# Funciones para medir tiempos de ejecucion
+
+def get_time():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+
+def delta_time(start, end):
+    """
+    devuelve la diferencia entre tiempos de procesamiento muestreados
+    """
+    elapsed = float(end - start)
+    return elapsed
+
+def get_memory():
+    """
+    toma una muestra de la memoria alocada en instante de tiempo
+    """
+    return tracemalloc.take_snapshot()
+
+
+def delta_memory(stop_memory, start_memory):
+    """
+    calcula la diferencia en memoria alocada del programa entre dos
+    instantes de tiempo y devuelve el resultado en bytes (ej.: 2100.0 B)
+    """
+    memory_diff = stop_memory.compare_to(start_memory, "filename")
+    delta_memory = 0.0
+
+    # suma de las diferencias en uso de memoria
+    for stat in memory_diff:
+        delta_memory = delta_memory + stat.size_diff
+    # de Byte -> kByte
+    delta_memory = delta_memory/1024.0
+    return delta_memory
--- App/model.py
+++ App/model.py
@@ -27,24 +27,175 @@
 
 import config as cf
 from DISClib.ADT import list as lt
+from DISClib.ADT import stack as st
+from DISClib.ADT import queue as qu
 from DISClib.ADT import map as mp
+from DISClib.ADT import minpq as mpq
+from DISClib.ADT import indexminpq as impq
+from DISClib.ADT import orderedmap as om
 from DISClib.DataStructures import mapentry as me
+from DISClib.ADT import graph as gr
+from DISClib.Algorithms.Graphs import scc
+from DISClib.Algorithms.Graphs import dijsktra as djk
+from DISClib.Algorithms.Graphs import bellmanford as bf
+from DISClib.Algorithms.Graphs import bfs
+from DISClib.Algorithms.Graphs import dfs
+from DISClib.Algorithms.Graphs import prim
 from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import insertionsort as ins
+from DISClib.Algorithms.Sorting import selectionsort as se
+from DISClib.Algorithms.Sorting import mergesort as merg
+from DISClib.Algorithms.Sorting import quicksort as quk
 assert cf
 
 """
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+Se define la estructura de un catálogo de videos. El catálogo tendrá
+dos listas, una para los videos, otra para las categorias de los mismos.
 """
 
 # Construccion de modelos
 
-# Funciones para agregar informacion al catalogo
+
+def new_data_structs():
+    """
+    Inicializa las estructuras de datos del modelo. Las crea de
+    manera vacía para posteriormente almacenar la información.
+    """
+    #TODO: Inicializar las estructuras de datos
+    pass
+
+
+# Funciones para agregar informacion al modelo
+
+def add_data(data_structs, data):
+    """
+    Función para agregar nuevos elementos a la lista
+    """
+    #TODO: Crear la función para agregar elementos a una lista
+    pass
+
 
 # Funciones para creacion de datos
 
+def new_data(id, info):
+    """
+    Crea una nueva estructura para modelar los datos
+    """
+    #TODO: Crear la función para estructurar los datos
+    pass
+
+
 # Funciones de consulta
+
+def get_data(data_structs, id):
+    """
+    Retorna un dato a partir de su ID
+    """
+    #TODO: Crear la función para obtener un dato de una lista
+    pass
+
+
+def data_size(data_structs):
+    """
+    Retorna el tamaño de la lista de datos
+    """
+    #TODO: Crear la función para obtener el tamaño de una lista
+    pass
+
+
+def req_1(data_structs):
+    """
+    Función que soluciona el requerimiento 1
+    """
+    # TODO: Realizar el requerimiento 1
+    pass
+
+
+def req_2(data_structs):
+    """
+    Función que soluciona el requerimiento 2
+    """
+    # TODO: Realizar el requerimiento 2
+    pass
+
+
+def req_3(data_structs):
+    """
+    Función que soluciona el requerimiento 3
+    """
+    # TODO: Realizar el requerimiento 3
+    pass
+
+
+def req_4(data_structs):
+    """
+    Función que soluciona el requerimiento 4
+    """
+    # TODO: Realizar el requerimiento 4
+    pass
+
+
+def req_5(data_structs):
+    """
+    Función que soluciona el requerimiento 5
+    """
+    # TODO: Realizar el requerimiento 5
+    pass
+
+
+def req_6(data_structs):
+    """
+    Función que soluciona el requerimiento 6
+    """
+    # TODO: Realizar el requerimiento 6
+    pass
+
+
+def req_7(data_structs):
+    """
+    Función que soluciona el requerimiento 7
+    """
+    # TODO: Realizar el requerimiento 7
+    pass
+
+
+def req_8(data_structs):
+    """
+    Función que soluciona el requerimiento 8
+    """
+    # TODO: Realizar el requerimiento 8
+    pass
+
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
+def compare(data_1, data_2):
+    """
+    Función encargada de comparar dos datos
+    """
+    #TODO: Crear función comparadora de la lista
+    pass
+
 # Funciones de ordenamiento
+
+
+def sort_criteria(data_1, data_2):
+    """sortCriteria criterio de ordenamiento para las funciones de ordenamiento
+
+    Args:
+        data1 (_type_): _description_
+        data2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    #TODO: Crear función comparadora para ordenar
+    pass
+
+
+def sort(data_structs):
+    """
+    Función encargada de ordenar la lista con los datos
+    """
+    #TODO: Crear función de ordenamiento
+    pass
