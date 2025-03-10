d5802921fca8226ddece5a50c3be25c797b4eec4
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-03-10 13:54:13 -0500

    Add mergesort implementation and corresponding unit tests

---
+++ Src/Func/Algorithms/Sorts/mergesort.py
@@ -0,0 +1,43 @@
+﻿from Src.Func.DataStructs.List import arlt
+from Src.Func.DataStructs.List import sllt
+from typing import Callable
+
+
+def sort(lt: dict, sort_crit: Callable) -> dict:
+    if lt["type"] == "ARRAYLIST":
+        lst = arlt
+    elif lt["type"] == "LINKEDLIST":
+        lst = sllt
+    size = lst.size(lt)
+    if size > 1:
+        mid = (size // 2)
+        _left_lt = lst.sub_list(lt, 0, mid)
+        _right_lt = lst.sub_list(lt, mid + 1, size - 1)
+        sort(_left_lt, sort_crit)
+        sort(_right_lt, sort_crit)
+        i = j = k = 0
+
+        _n_left = lst.size(_left_lt)
+        _n_right = lst.size(_right_lt)
+
+        while (i < _n_left) and (j < _n_right):
+            elemi = lst.get_element(_left_lt, i)
+            elemj = lst.get_element(_right_lt, j)
+            if sort_crit(elemj, elemi):
+                lst.update(lt, k, elemj)
+                j += 1
+            else:
+                lst.update(lt, k, elemi)
+                i += 1
+            k += 1
+
+        while i < _n_left:
+            lst.update(lt, k, lst.get_element(_left_lt, i))
+            i += 1
+            k += 1
+
+        while j < _n_right:
+            lst.update(lt, k, lst.get_element(_right_lt, j))
+            j += 1
+            k += 1
+    return lt
---
+++ test_sort.py
@@ -0,0 +1,27 @@
+﻿from Src.Func.DataStructs.List import arlt
+from Src.Func.DataStructs.List import sllt
+from Src.Func.Algorithms.Sorts.mergesort import sort as mst
+from typing import Callable
+import random
+
+
+def sort_crit_by_idx(a: dict, b: dict) -> bool:
+    # print(a, b)
+    return a["idx"] <= b["idx"]
+
+
+a = arlt.new_array_lt()
+
+for i in range(1, 27):
+    idx = random.randint(13 * i, 42 * (i + 1))
+    arlt.add_last(a, {"id": i,
+                      "idx": idx,
+                      "name": chr(96 + i)})
+
+print(a)
+
+a = mst(a, sort_crit_by_idx)
+
+
+for elm in arlt.iterator(a):
+    print(elm)
